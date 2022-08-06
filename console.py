e4cf7776300ef4cdf074f125d724e5995abc31aa
ShagizMan <danielgetaneh2011@gmail.com>
2022-08-06 23:12:52 +0300

    fix the problem

--- console.py
+++ console.py
@@ -31,23 +31,6 @@ class HBNBCommand(cmd.Cmd):
             "Review"
             }
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
-
     def default(self, arg):
         """ Improves the default cmd. """
         argdict = {
@@ -215,6 +198,24 @@ def parse(arg):
             if argl[0] == obj.__class__.__name__:
                 count += 1
         print(count)
+
+
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 
 if __name__ == '__main__':
